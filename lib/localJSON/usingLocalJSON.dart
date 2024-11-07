9f566158f53e8ac7013db3f97fb8390ac96f7026
korolenko danila <korolenko.danila1@gmail.com>
2024-11-07 15:40:43 +0300

    something new

---
+++ lib/localJSON/usingLocalJSON.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+class MyData {
+  final String items = '{"data": [ '
+      '{"title": "January"},'
+      '{"title": "February"},'
+      '{"title": "March"} '
+      '] }';
+}
+
+class DataSeries {
+}
